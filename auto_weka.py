81308b14d3f2995553508d6a32f2155ef233191e
Rilyaus <rilyaus.gd@gmail.com>
2018-06-19 17:13:29 +0900

    Variable separate

--- auto_weka.py
+++ auto_weka.py
@@ -1,7 +1,14 @@
 import os
 import subprocess
 
-cmd = 'java -cp "$CLASSPATH" weka.classifiers.functions.SMOreg -C 1.0 -N 0 -classifications "weka.classifiers.evaluation.output.prediction.CSV -decimals 5" -I "weka.classifiers.functions.supportVector.RegSMOImproved -T 0.001 -V -P 1.0E-12 -L 0.001 -W 1" -K "weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007" -t "./D2/lat_Train_case01.arff" -T "./D2/lat_Test_case01.arff" -c "1" -p 0'
+
+tec = 'weka.classifiers.functions.SMOreg -C 1.0 -N 0'
+classifications = 'weka.classifiers.evaluation.output.prediction.CSV -decimals 5'
+option_I = 'weka.classifiers.functions.supportVector.RegSMOImproved -T 0.001 -V -P 1.0E-12 -L 0.001 -W 1'
+option_K = 'weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007'
+traingSet = './D2/lat_Train_case01.arff'
+testSet = './D2/lat_Test_case01.arff'
+cmd = 'java -cp "$CLASSPATH" %s -classifications "%s" -I "%s" -K "%s" -t "%s" -T "%s" -c "1" -p 0' %(tec, classifications, option_I, option_K, traingSet, testSet)
 # > ".\D2_case01_lat.csv"
 
 #result = subprocess.check_output(cmd, stdout=subprocess.PIPE).stdout
